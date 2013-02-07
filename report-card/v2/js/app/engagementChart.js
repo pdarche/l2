fff5e32d3f663eab175c3933a904dfe463e87acb
Peter Darche <pdarche@gmail.com>
2013-02-07 15:37:05 -0500

    add engagement chart brand toggle

--- report-card/v2/js/app/engagementChart.js
+++ report-card/v2/js/app/engagementChart.js
@@ -1,32 +1,68 @@
 
 var EngagementView = {
 
+    clickedBenchmarks : [],
+
 	init : function() {
 
 		var source = $('#engagement_view').html() 
 	    var template = Handlebars.compile( source )
 	    $('#module_container').html( template )
 
-	    //fetch user brand
-	    EngagementView.configUserObject( user.users[0].default_brand_id )
-	    console.log( "the user's data is: ", engagementBrandData )
-	    $.when( EngagementView.fetch( "GET", "data", brandNonEngagement ) )
-	    	.done( function( data ){ console.log("this is the non-engagement data ", data) } )
-	    	.done( EngagementView.fetch( "GET", "data", engagementBrandData ) )
-	    	.done( function( data ){ console.log("this is the engagement data ", data) } )
+	    //fetch brands data
+        $.when(
+            EngagementView.fetch( "GET", "data", brandDataEngagement ), 
+            EngagementView.fetch( "GET", "data", categoryDataEngagement ) 
+        ).done(
+            function( memberBrand, categoryLeaders ){
 
+                EngagementView.clickedBenchmarks.push( memberBrand[0].brands[0] )
+                console.log( "these are the clickedBnechies after first return ", EngagementView.clickedBenchmarks )
+
+
+                brandDataEngagement.brands = []
+                $.each(categoryLeaders[0].brands, function(i){                    
+                    brandDataEngagement.brands.push( { "brand_id" : categoryLeaders[0].brands[i].brand_id } )
+                })
+
+
+                $.when( EngagementView.fetch( "GET", "data", brandDataEngagement ) )
+                .done(
+                    function( categoryLeaders ) {
+                        console.log( "the category leaders are ", categoryLeaders.brands )
+                        var brands = [ EngagementView.clickedBenchmarks, categoryLeaders.brands ] 
+
+                        console.log("the brands array is ", brands)
+
+                        var brandList = []
+                        $.each(brands, function(i){
+                            $.each(brands[i], function(j){
+                                brandList.push(brands[i][j])
+                            })
+                        })
+
+                        EngagementView.clickedBenchmarks = brandList
+
+                        var engagementData = EngagementView.formatData( brandList )
+
+                        EngagementView.renderChart( engagementData )
+                    }
+                )
+            }
+        )
+
+        //populate/bind 
 		if ( brandList.length > 0 ){
             
             console.log("already set")
             TimeseriesView.bindAutocomplete()
 
-
         } else {
         	console.log("not yet set, setting")
 
-            // $.when(TimeseriesView.fetch("GET", "ref", getAllBrands, this))
-            //     .done( TimeseriesView.setBrandList )
-            //     .done( TimeseriesView.bindAutocomplete )
+            $.when(TimeseriesView.fetch("GET", "ref", getAllBrands, this))
+                .done( TimeseriesView.setBrandList )
+                .done( TimeseriesView.bindAutocomplete )
 
         }
 
@@ -38,11 +74,54 @@ var EngagementView = {
         $.when( TimeseriesView.renderFavoriteBrands( user.users[0].favorite_brands) )
             .done( TimeseriesView.synchFavorites )
 
+        EngagementView.bindEvents()
+
 	},
 
-	renderChart : function( callback ) {
+    bindEvents : function() {
 
-		EngagementView.getData(3, callback)
+        $('#category_benchmark_results, #favorite_benchmarks_results, #search_benchmarks_results').delegate('.brand-check', 'click', function(){
+            var id = $(this).attr('class').split(" ")[1]
+            
+            if ( $(this).is(':checked') ){
+
+                $.when( EngagementView.configUserObject( id ) )
+                .done(
+                    function(data){ console.log ("returned query object ", data )},
+                    $.when( EngagementView.fetch( "GET", "data", brandDataEngagement, this) )
+                    .done(
+                        function( data ){
+                            EngagementView.clickedBenchmarks.push( data.brands[0] )
+                            var engagementData = EngagementView.formatData( EngagementView.clickedBenchmarks )
+                            EngagementView.renderChart( engagementData )
+                        }
+
+                    )
+                )
+                
+            } else {
+
+                EngagementView.removeFromBenchmarks( id )
+                var data = EngagementView.formatData( EngagementView.clickedBenchmarks )
+                EngagementView.renderChart( data )                
+            }
+        })
+
+    },
+
+    removeFromBenchmarks : function( brandId ){
+
+        var index = $.inArray( EngagementView.clickedBenchmarks, brandId )
+
+        EngagementView.clickedBenchmarks.remove( index )
+
+    }, 
+
+	renderChart : function( engagementData ) {
+
+        $('#engagement_chart_container').children().remove()
+        engagementChart(engagementData, 12, '#engagement_chart_container')
+        $('#engagement_chart_container').children().hide().fadeIn()
 
 	},
 
@@ -90,6 +169,8 @@ var EngagementView = {
 
 		//make request
 		if ( method === "GET" ){
+            console.log("query string: ", queryString)
+
 			return $.getJSON( query )
 		}
 		else if ( method === "POST" ){
@@ -104,91 +185,95 @@ var EngagementView = {
 			    	console.log("yuuccchhh", r)
 			    }
 			});
-
 		}
 
     },
 
-	getData : function ( category, callback ){
+    formatData : function( brandsAr ){
 
-		var url = 'http://l2-data.com/ajax/scraperDataWS.svc/getTop10EngagementBrandDataBySize?u=6700' 
-			url += '&categoryId=3&likesMin=500001'
-			url += '&likesMax=1000000&callback=?'
+        console.log("these are the brands", brandsAr)
 
-        return $.getJSON( url, function(data) {
+        var data = []
 
-        	var returnedData = data;
+        for ( i = 0; i < brandsAr.length; i++ ) {
 
-	        var data = [],
-	            minX,
-	            maxX,
-	            minY,
-	            maxY,
-	            numCircles = 5;
+              x = brandsAr[i].facebook_likes_count_total_growth30[0][1],
+              y = brandsAr[i].facebook_likes_interaction_rate[0][1]/100,
+              d = brandsAr[i].facebook_likes_count_total[0][1],
+              name = brandsAr[i].brandfamily_name,
+              id = brandsAr[i].brand_id;
 
-            //if there are less than 5 circles, set number of circles to number of brands returned
-            returnedData.length < 5 ? numCircles = returnedData.length : null
+              data.push([ x, y, d, name, id ])      
+        }
 
-        	for ( i = 0; i < numCircles; i++ ) {
-
-		          x = (roundNumber(parseFloat(returnedData[i].likesGrowthPct30), 2)/100),
-		          y = (roundNumber(parseFloat(returnedData[i].FBEngagement), 2)/100),
-		          d = parseInt(returnedData[i].likesTotal.replace(/,/g , "")),
-		          name = returnedData[i].brandName;
-		          id = String(returnedData[i].brandId);
-
-		            data.push([ x, y, d, name, id ])      
-		      }
-
-        	engagementChart(data, 12, '#engagement_chart_container')
-
-        } )
-
+        return data
     },
 
     configTopEightEngagement : function( categoryId, sizeId ){	
+
+
 
     },
 
     configUserObject : function( brandId ){
 
-    	brandNonEngagement.brands[0].brand_id = "74" //brandId
-    	brandNonEngagement.fact_brand_daily.constraints.start_date = (1).year().ago().toString("yyyyMMdd")
-  		brandNonEngagement.fact_brand_daily.constraints.end_date = Date.today().toString("yyyyMMdd")
+        brandDataEngagement.brands = []
+    	brandDataEngagement.brands.push( { "brand_id" : brandId})
 
-  		engagementBrandData.brands[0].brand_id = "74" //brandId
-    	engagementBrandData.fact_fbpost.constraints.start_date = (1).year().ago().toString("yyyyMMdd")
-  		engagementBrandData.fact_fbpost.constraints.end_date = Date.today().toString("yyyyMMdd")
+        return brandDataEngagement
 
     }
 
 }
 
+function round2(number){
+    return Math.round( number*100 )/100 
+}
 
-var brandNonEngagement = {
-    "brands" : [{ "brand_id" : "" }],
+
+var brandDataEngagement = {
+    "brands" : [{ "brand_id" : "74" }],
     "fact_brand_daily" : {
         "metrics": [
-            "facebook_likes_count_total",
-            "facebook_likes_count_total_growth30"
+            "facebook_likes_count_total",    
+            "facebook_likes_count_total_growth30",
+            "facebook_likes_interaction_rate"
         ],
         "constraints" : {
-            "start_date" : "",
-            "end_date" : "",
+            "start_date" : (3).days().ago().toString("yyyyMMdd"),
+            "end_date" : (2).days().ago().toString("yyyyMMdd"),
             "timeseries" : false
         }
     }
 }
 
+var categoryDataEngagement = {
+    "brands" : [{ "category_id" : "3" }],
+    "fact_brand_daily" : {
+        "metrics": [
+            "facebook_likes_interaction_rate"           
+        ],
+        "constraints" : {
+            "start_date" : (3).days().ago().toString("yyyyMMdd"),
+            "end_date" : (2).days().ago().toString("yyyyMMdd"),
+            "timeseries" : false,
+            "facebook_likes_interaction_rate" : {
+                "top" : 8
+            }
+        }
+    }
+}
+
+
 var engagementBrandData = {
-    "brands" : [{ "brand_id" : "" }],
+    "brands" : [{ "brand_id" : "74" }],
     "fact_fbpost" : {
         "metrics": [
             "facebook_like_interaction_rate"
         ],
         "constraints" : {
-            "start_date" : "",
-            "end_date" : "",
+            "start_date" : (3).year().ago().toString("yyyyMMdd"),
+            "end_date" : (2).year().ago().toString("yyyyMMdd"),
             "timeseries" : false
         }
     }
